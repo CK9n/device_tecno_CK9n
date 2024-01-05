f6811f8e9a33ab7794a4981a24f3aa79ebc39f11
AndroidHQ254 <d.murimi14@gmail.com>
2024-01-05 21:16:25 +0300

    CK9n: Initial device skeleton tree

---
+++ BoardConfig.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/tecno/CK9n
+
+# Inherit the proprietary files
+include vendor/tecno/CK9n/BoardConfigVendor.mk
---
+++ README.md
@@ -0,0 +1,9 @@
+# Device tree for TECNO CAMON 20 Premier 5G
+
+```
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
